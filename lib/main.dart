6d97df75639288c448ba9674fbac5006288394db
liudongjun <3170495286@qq.com>
2023-06-29 10:45:23 +0800

    fixbug:空状态不能拖拽

--- lib/main.dart
+++ lib/main.dart
@@ -50,49 +50,85 @@ class _DragAndDropState extends State<DragAndDrop> {
           Expanded(
               child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: _leftItems
-                .asMap()
-                .map((index, item) => MapEntry(
-                    index,
-                    LongPressDraggable(
-                      data: item,
-                      feedback: Container(
-                        width: 100,
-                        height: 100,
-                        color: Colors.blue.withOpacity(0.5),
-                        child: Center(
-                          child: Text('${item.name}',
-                              style:
-                                  TextStyle(fontSize: 10, color: Colors.white)),
-                        ),
-                      ),
-                      childWhenDragging: Container(
-                        //拖拽后替换元素
-                        width: 100,
-                        height: 100,
-                        color: Colors.blue.withOpacity(0.5),
-                        child: Image.asset('assets/images/${item.imgSrc}'),
-                      ),
+            children: _leftItems.length > 0
+                ? _leftItems
+                    .asMap()
+                    .map((index, item) => MapEntry(
+                        index,
+                        LongPressDraggable(
+                          data: item,
+                          feedback: Container(
+                            width: 100,
+                            height: 100,
+                            color: Colors.blue.withOpacity(0.5),
+                            child: Center(
+                              child: Text('${item.name}',
+                                  style: TextStyle(
+                                      fontSize: 10, color: Colors.white)),
+                            ),
+                          ),
+                          childWhenDragging: Container(
+                            //拖拽后替换元素
+                            width: 100,
+                            height: 100,
+                            color: Colors.blue.withOpacity(0.5),
+                            child: Image.asset('assets/images/${item.imgSrc}'),
+                          ),
+                          child: DragTarget<Item>(
+                            onAccept: (data) {
+                              // 处理排序 和新增删除逻辑
+                              print('accept index $data');
+                              print('is: ${_leftItems.contains(data)}');
+                              if (_leftItems.contains(data)) {
+                                setState(() {
+                                  // final temp = widget._list[data];
+                                  _leftItems.remove(data);
+                                  _leftItems.insert(index, data);
+                                });
+                              } else {
+                                setState(() {
+                                  _leftItems.insert(index, data);
+                                  _rightItems.remove(data);
+                                });
+                              }
+                            },
+                            onWillAccept: (data) {
+                              print(
+                                  'current index: $index, on will accept $data');
+                              return true;
+                            },
+                            onLeave: (data) {
+                              // print('index $index , leave $data');
+                            },
+                            builder: (context, data, rejects) {
+                              print('builder = $data,  rejects= $rejects');
+                              return Container(
+                                child: Container(
+                                  width: 100,
+                                  height: 100,
+                                  color: Colors.red.withOpacity(0.5),
+                                  child: Center(
+                                    child: Text('${item.name}'),
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        )))
+                    .values
+                    .toList()
+                : [
+                    Container(
                       child: DragTarget<Item>(
                         onAccept: (data) {
                           // 处理排序 和新增删除逻辑
                           print('accept index $data');
-                          print('is: ${_leftItems.contains(data)}');
-                          if (_leftItems.contains(data)) {
-                            setState(() {
-                              // final temp = widget._list[data];
-                              _leftItems.remove(data);
-                              _leftItems.insert(index, data);
-                            });
-                          } else {
-                            setState(() {
-                              _leftItems.insert(index, data);
-                              _rightItems.remove(data);
-                            });
-                          }
+                          setState(() {
+                            _leftItems.insert(0, data);
+                            _rightItems.remove(data);
+                          });
                         },
                         onWillAccept: (data) {
-                          print('current index: $index, on will accept $data');
                           return true;
                         },
                         onLeave: (data) {
@@ -106,95 +142,131 @@ class _DragAndDropState extends State<DragAndDrop> {
                               height: 100,
                               color: Colors.red.withOpacity(0.5),
                               child: Center(
-                                child: Text('${item.name}'),
+                                child: Text('空'),
                               ),
                             ),
                           );
                         },
                       ),
-                    )))
-                .values
-                .toList(),
+                    )
+                  ],
           )),
           Expanded(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: _rightItems
-                .asMap()
-                .map((index, item) => MapEntry(
-                    index,
-                    LongPressDraggable(
-                      data: item,
-                      feedback: Container(
-                        width: 100,
-                        height: 100,
-                        color: Colors.blue.withOpacity(0.5),
-                        child: Center(
-                          child: Text('${item.name}',
-                              style:
-                                  TextStyle(fontSize: 10, color: Colors.white)),
-                        ),
-                      ),
-                      childWhenDragging: Container(
-                          width: 100,
-                          height: 100,
-                          color: Colors.blue.withOpacity(0.5),
-                          child: Image.asset('assets/images/${item.imgSrc}')),
-                      onDragStarted: () {
-                        print('onDragStarted');
-                        // setState(() {
-                        //   _draggedIndex = index;
-                        // });
-                      },
-                      onDragEnd: (dragDetails) {
-                        print('dragDetails:$dragDetails');
-                        // setState(() {
-                        //   _draggedIndex = null;
-                        // });
-                      },
-                      child: DragTarget<Item>(
-                        onAccept: (data) {
-                          // 处理排序 和新增删除逻辑
-                          print('accept index $data');
-                          print('is: ${_rightItems.contains(data)}');
-                          if (_rightItems.contains(data)) {
-                            setState(() {
-                              // final temp = widget._list[data];
-                              _rightItems.remove(data);
-                              _rightItems.insert(index, data);
-                            });
-                          } else {
-                            setState(() {
-                              _rightItems.insert(index, data);
-                              _leftItems.remove(data);
-                            });
-                          }
-                        },
-                        onWillAccept: (data) {
-                          print('current index: $index, on will accept $data');
-                          return true;
-                        },
-                        onLeave: (data) {
-                          print('index $index , leave $data');
-                        },
-                        builder: (context, data, rejects) {
-                          print('builder = $data,  rejects= $rejects');
-                          return Container(
-                            child: Container(
-                              width: 100,
-                              height: 100,
-                              color: Colors.red.withOpacity(0.5),
-                              child: Center(
-                                child: Text('${item.name}'),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: _rightItems.length > 0
+                    ? _rightItems
+                        .asMap()
+                        .map((index, item) => MapEntry(
+                            index,
+                            LongPressDraggable(
+                              data: item,
+                              feedback: Container(
+                                width: 100,
+                                height: 100,
+                                color: Colors.blue.withOpacity(0.5),
+                                child: Center(
+                                  child: Text('${item.name}',
+                                      style: TextStyle(
+                                          fontSize: 10, color: Colors.white)),
+                                ),
                               ),
-                            ),
-                          );
-                        },
-                      ),
-                    )))
-                .values
-                .toList(),
-          )),
+                              childWhenDragging: Container(
+                                  width: 100,
+                                  height: 100,
+                                  color: Colors.blue.withOpacity(0.5),
+                                  child: Image.asset(
+                                      'assets/images/${item.imgSrc}')),
+                              onDragStarted: () {
+                                print('onDragStarted');
+                                // setState(() {
+                                //   _draggedIndex = index;
+                                // });
+                              },
+                              onDragEnd: (dragDetails) {
+                                print('dragDetails:$dragDetails');
+                                // setState(() {
+                                //   _draggedIndex = null;
+                                // });
+                              },
+                              child: DragTarget<Item>(
+                                onAccept: (data) {
+                                  // 处理排序 和新增删除逻辑
+                                  print('accept index $data');
+                                  print('is: ${_rightItems.contains(data)}');
+                                  if (_rightItems.contains(data)) {
+                                    setState(() {
+                                      // final temp = widget._list[data];
+                                      _rightItems.remove(data);
+                                      _rightItems.insert(index, data);
+                                    });
+                                  } else {
+                                    setState(() {
+                                      _rightItems.insert(index, data);
+                                      _leftItems.remove(data);
+                                    });
+                                  }
+                                },
+                                onWillAccept: (data) {
+                                  print(
+                                      'current index: $index, on will accept $data');
+                                  return true;
+                                },
+                                onLeave: (data) {
+                                  print('index $index , leave $data');
+                                },
+                                builder: (context, data, rejects) {
+                                  print('builder = $data,  rejects= $rejects');
+                                  return Container(
+                                    child: Container(
+                                      width: 100,
+                                      height: 100,
+                                      color: Colors.red.withOpacity(0.5),
+                                      child: Center(
+                                        child: Text('${item.name}'),
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ),
+                            )))
+                        .values
+                        .toList()
+                    : [
+                        Container(
+                          child: DragTarget<Item>(
+                            onAccept: (data) {
+                              print('accept index $data');
+                              // 处理排序 和新增删除逻辑
+                              setState(() {
+                                _rightItems.insert(0, data);
+                                _leftItems.remove(data);
+                              });
+                            },
+                            onWillAccept: (data) {
+                              // print('current index: $index, on will accept $data');
+                              return true;
+                            },
+                            onLeave: (data) {
+                              // print('index $index , leave $data');
+                            },
+                            builder: (context, data, rejects) {
+                              print('builder = $data,  rejects= $rejects');
+                              return Container(
+                                child: Container(
+                                  width: 100,
+                                  height: 100,
+                                  color: Colors.red.withOpacity(0.5),
+                                  child: Center(
+                                    child: Text('空'),
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        )
+                      ]),
+          ),
         ],
       ),
     );
